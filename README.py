342be26557005657bcd1455eb7c92a90706211c0
Thomas Chesseboeuf <thomas.chesseboeuf@etu.minesparis.psl.eu>
2022-10-27 11:13:34 +0200

    Correction du readme

--- README.py
+++ README.py
@@ -1,5 +1,8 @@
-how_many_people = 4
-
-for i in range(1, how_many_people + 1):
-    print(f"{i:02d} Nom:")
-    print(f"{i:02d} github:")
+01 Nom:
+01 github:
+02 Nom:
+02 github:
+03 Nom:
+03 github:
+04 Nom:
+04 github:
